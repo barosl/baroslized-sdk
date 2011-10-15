a361fe2917c7ecbcee908416aac4fc895a60442e
Barosl LEE <git@barosl.com>
2011-10-15 12:19:27 +0900

    initial release

---
+++ android/config.mk
@@ -0,0 +1,5 @@
+include /home/barosl/.sdk/baroslized-android/global-config.mk
+
+name := inkl_cast
+main := barosl.inkl_cast/.ChannelList
+default := release
---
+++ android/global-config.mk
@@ -0,0 +1,3 @@
+include /home/barosl/.sdk/baroslized-android/.keys/config.mk
+
+NDK_PATH = /home/barosl/.sdk/android-ndk-1.6_r1
